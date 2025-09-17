f32ebc24096c80b5cd775e78582638933d4466d1
Dajungeks <dajungeks@gmail.com>
2025-09-17 18:14:57 +0700

    Update approval_page.py

--- pages/approval_page.py
+++ pages/approval_page.py
@@ -1,9 +1,4 @@
 import streamlit as st
-st.write("🔴 approval_page.py 파일 실행됨!")
-
-
-
-import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 import plotly.express as px
